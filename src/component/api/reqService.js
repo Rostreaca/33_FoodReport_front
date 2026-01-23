b1dc4ce9b0ce0cebf47333b91953e66574ab8488
dltbxkfm86-creator <dltbxkfm86@gmial.com>
2026-01-23 15:17:21 +0900

    feat:기본인스턴스분리

---
+++ src/component/api/reqService.js
@@ -0,0 +1,31 @@
+import axios from "axios";
+
+const API_URL = window.ENV?.API_URL;
+
+// 인증이 필요한 요청용 인스턴스
+const authInstance = axios.create({
+  baseURL: API_URL, // 기본 url
+  headers: {
+    "Content-Type": "application/json", // 기본 헤더
+  },
+});
+
+// 인증이 불필요한 요청용 인스턴스
+const publicInstance = axios.create({
+  baseURL: API_URL,
+  headers: {
+    "Content-Type": "application/json", // 기본 헤더
+  },
+});
+
+// // 인증 인터셉터 추가
+// authInstance.interceptors.request.use(
+//   (config) => {
+//     const token = localStorage.getItem("accessToken");
+//     if (token) {
+//       config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
