09216315c683f55c226a01e3869868a939c3afa8
Rostreaca <freestalere@naver.com>
2026-02-06 10:18:01 +0900

    feat : 리뷰 게시글 좋아요 요청 코드 작성
    - 리뷰 게시글 좋아요 요청 코드 작성
    - 좋아요 성공 / 실패 시 토스트 출력
    - 좋아요를 이미 눌렀을 경우 좋아요 제거 / 아닐경우 좋아요 추가
    - 좋아요를 누른 댓글의 좋아요 버튼 모양 변경

--- src/component/Review/ReviewUpdateForm.jsx
+++ src/component/Review/ReviewUpdateForm.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useContext } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Image as ImageIcon, X , Home, ChevronRight } from 'lucide-react';
 import {
